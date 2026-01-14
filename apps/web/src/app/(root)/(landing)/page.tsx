4d1a138c66865c727dce86ff88c365c317ea90b2
binit <binitgupta.1711@gmail.com>
2026-01-14 17:53:28 +0530

    Landing page first draft complete

--- apps/web/src/app/(root)/(landing)/page.tsx
+++ apps/web/src/app/(root)/(landing)/page.tsx
@@ -3,8 +3,17 @@ import HeroTitle from '@/components/landing-page/hero-title'
 
 const page = () => {
   return (
-    <div className='w-screen h-screen flex flex-col justify-center items-center'>
-      <HeroTitle />
+    <div className='w-screen min-h-screen flex flex-col items-center'>
+      {/* Spacer for fixed navbar */}
+      <div className='h-26.5' />
+      
+      {/* Hero Section */}
+      <div className='w-full flex flex-col justify-center items-center '>
+        <HeroTitle />
+      </div>
+
+      {/* Test scroll content */}
+      
     </div>
   )
 }
--- apps/web/src/components/landing-page/navbar/navbar.jsx
+++ apps/web/src/components/landing-page/navbar/navbar.jsx
@@ -5,28 +5,25 @@ import '../../../app/globals.css'
 
 const Navbar = () => {
   return (
-    <div className='relative w-full h-30 flex justify-center items-center bg-background'>
-      
-      
-      <div className="hidden lg:flex lg:flex-row lg:absolute h-[87.5%] top-0 w-225 left-1/2 -translate-x-1/2 border-x-2 border-[#1f1f1f] items-center px-6 justify-between">
-        <div className='flex pb-5 -ml-6 items-center h-full'>
+    <div className='fixed top-0 left-0 z-50 w-full h-26.5 flex justify-center items-center bg-background'>
+      <div className="hidden lg:flex lg:flex-row lg:absolute h-full top-0 w-225 left-1/2 -translate-x-1/2 border-x-2 border-[#1f1f1f] items-center px-6 justify-between">
+        <div className='flex pb-5 items-center h-full'>
           <Logo />
         </div>
         <div className='flex items-center gap-6'>
           <p className='cursor-pointer font-plus-jakarta-sans italic font-bold'>DOCS</p>
-          <p className='cursor-pointer font-plus-jakarta-sans italic font-bold'>GET STARTED</p>
+          <p className='cursor-pointer font-plus-jakarta-sans italic font-bold'>DIVE IN</p>
         </div>
       </div>
-      <div className="lg:hidden flex flex-row absolute w-screen h-[87.5%] top-0  left-1/2 -translate-x-1/2 border-x-2 border-[#1f1f1f] items-center px-6 justify-between">
+      <div className="lg:hidden flex flex-row absolute w-screen h-full top-0 left-1/2 -translate-x-1/2 border-x-2 border-[#1f1f1f] items-center px-6 justify-between">
         <div className='flex pb-5 -ml-6 items-center h-full'>
           <Logo />
         </div>
         <div className='flex items-center gap-6 text-[12px]'>
           <p className='cursor-pointer font-plus-jakarta-sans italic font-bold'>DOCS</p>
-          <p className='cursor-pointer font-plus-jakarta-sans italic font-bold'>GET STARTED</p>
+          <p className='cursor-pointer font-plus-jakarta-sans italic font-bold'>DIVE IN</p>
         </div>
       </div>
-
       <FlowingLine className="absolute top-26.5 w-full " />
     </div>
   )
