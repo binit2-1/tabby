81edec27a9facfaf3564f9e5c47d1996e2c972b1
binit <binitgupta.1711@gmail.com>
2026-01-15 12:51:39 +0530

    >Add a Dialog box for code addition

--- apps/web/src/app/(root)/(tool)/create/page.tsx
+++ apps/web/src/app/(root)/(tool)/create/page.tsx
@@ -1,11 +1,11 @@
 "use client"
 import CodeEditor from '@/components/create-page/code-editor'
 import { RightSideBar } from '@/components/create-page/ui-blocks/rigth-sidebar'
+import LeftSideBar from '@/components/create-page/ui-blocks/left-sidebar'
 import { useState } from 'react'
 
 
 const Page = () => {
-  // use the same keys as the sidebar/framework list (lowercase)
   const [language, setLanguage] = useState<string>('javascript')
 
   return (
@@ -15,6 +15,10 @@ const Page = () => {
             <div className='flex flex-1 justify-center items-center mt-26.5'>
                 <CodeEditor language={language} onLanguageChange={setLanguage} />
             </div>
+        </div>
+        {/* Left sidebar between inner and outer left borders */}
+        <div className='absolute left-92 top-30 bottom-0'>
+          <LeftSideBar />
         </div>
         {/* Right sidebar between inner and outer right borders */}
         <div className='absolute right-81 top-30 bottom-0'>
--- apps/web/src/components/create-page/ui-blocks/left-sidebar.tsx
+++ apps/web/src/components/create-page/ui-blocks/left-sidebar.tsx
@@ -1,9 +1,56 @@
-import React from 'react'
+"use client"
+import { Button } from "@/components/ui/button"
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { useState } from "react"
+import { Plus } from "@phosphor-icons/react"
 
-const LeftSidebar = () => {
+export default function LeftSideBar() {
+
+  const [codeName, setCodeName] = useState<string>("")
+  const [description, setDescription] = useState<string>("")
   return (
-    <div>leftSidebar</div>
+    <Dialog>
+      <form>
+        <DialogTrigger asChild>
+          <Button variant="outline">Add <Plus size={16} /></Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-106.25">
+          <DialogHeader>
+            <DialogTitle>Code Information</DialogTitle>
+            <DialogDescription>
+              Add the minimal details about your code with a valid trigger word and you&apos;re
+              done.
+            </DialogDescription>
+          </DialogHeader>
+          <div className="grid gap-4">
+            <div className="grid gap-3">
+              <Label htmlFor="code-name-1">Code Name(Trigger Word)</Label>
+              <Input id="code-name-1" name="codeName" placeholder="e.g; Breadth First Search" />
+            </div>
+            <div className="grid gap-3">
+              <Label htmlFor="short-description-1">Short Description</Label>
+              <Input id="short-description-1" name="description" placeholder="e.g; This code applies breadth first search" />
+            </div>
+          </div>
+          <DialogFooter>
+            <DialogClose asChild>
+              <Button variant="outline">Cancel</Button>
+            </DialogClose>
+            <Button type="submit">Save changes</Button>
+          </DialogFooter>
+        </DialogContent>
+      </form>
+    </Dialog>
   )
 }
-
-export default LeftSidebar
