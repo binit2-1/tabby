49132cee575011545f392a14635e24235a7657d9
binit <binitgupta.1711@gmail.com>
2026-01-17 00:57:02 +0530

    >Redis services defined

---
+++ apps/api/src/services/redisServices.ts
@@ -0,0 +1,45 @@
+import { Redis } from "ioredis";
+
+const client = new Redis(process.env.REDIS_URL || "redis://localhost:6379");
+
+const generatedId = () => {
+  Math.random().toString(36).substring(2, 9);
+};
+
+export const addToBundle = async (
+  bundleId: string,
+  code: string,
+  ttlsec: number
+) => {
+  const snippetId = `snippet"${generatedId()}`;
+  const bundleKey = `bundle:${bundleId}`;
+
+  await client.set(snippetId, code, "EX", ttlsec);
+
+  await client.rpush(bundleKey, snippetId);
+
+  await client.expire(bundleKey, ttlsec);
+
+  return snippetId;
+};
+
+
+export const getBundle = async(bundleId:string)=>{
+    const bundleKey = `bundle:${bundleId}`;
+
+    const snippetIds = await client.lrange(bundleKey, 0, -1);
+
+    if(snippetIds.length === 0){
+        return null;
+    }
+
+    const snippets = await client.mget(snippetIds);
+
+    const parsedData = snippets.map((snippet) => {
+        if(snippet){
+            JSON.parse(snippet)
+        }
+    })
+    return parsedData;
+
+}
