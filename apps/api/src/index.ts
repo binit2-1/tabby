dd612dfc9c0b8e943fc1f6487005d9a6741eea71
binit <binitgupta.1711@gmail.com>
2026-01-18 01:10:07 +0530

    fix: correct helmet import and usage in index.ts; update verbatimModuleSyntax in tsconfig.json

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,5 +1,5 @@
 import express, { Router } from 'express';
-import * as helmet from 'helmet';
+import helmet from 'helmet';
 import cors from 'cors';
 import router from './routes/snippetRoutes.ts';
 
@@ -7,7 +7,7 @@ const app:express.Application = express();
 const PORT = process.env.PORT || 4000;
 
 //Middleware
-app.use(helmet.default());
+app.use(helmet());
 
 //cors
 app.use(cors({
